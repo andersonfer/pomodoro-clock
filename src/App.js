06d16cbdd106f4b6bfc8a27bbaa0e98751e13404
Anderson Fér <andersonfer@gmail.com>
2023-04-14 09:28:20 +0100

    Import the code from codepen.io

--- src/App.js
+++ src/App.js
@@ -1,25 +1,399 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MAX_LENGTH_IN_MINUTES = 60;
+const MIN_LENGTH_IN_MINUTES = 1;
+const FIFTY_NINE_SECONDS = 59;
+const ONE_SECOND_IN_MILISECONDS = 1000;
+const DEFAULT_BREAK_LENGTH = 5;
+const DEFAULT_SESSION_LENGTH = 25;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      breakLength: DEFAULT_BREAK_LENGTH,
+      sessionLength: DEFAULT_SESSION_LENGTH
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("resetClock", () => {
+      this.resetSetup();
+    });
+  }
+
+  resetSetup = () => {
+    this.setState({
+      breakLength: DEFAULT_BREAK_LENGTH,
+      sessionLength: DEFAULT_SESSION_LENGTH
+    });
+    this.dispatchNewSessionLength(DEFAULT_SESSION_LENGTH);
+  };
+
+  render() {
+    return (
+      <>
+        <div id="header">
+          <div id="title">25 + 5 clock</div>
+          <div id="clock-setup">
+            <BreakSetup
+              breakLength={this.state.breakLength}
+              incrementFnc={this.incrementBreakLength}
+              decrementFnc={this.decrementBreakLength}
+            />
+            <SessionSetup
+              sessionLength={this.state.sessionLength}
+              incrementFnc={this.incrementSessionLength}
+              decrementFnc={this.decrementSessionLength}
+            />
+          </div>
+        </div>
+
+        <div id="clock">
+          <Clock
+            sessionLength={this.state.sessionLength}
+            breakLength={this.state.breakLength}
+          />
+        </div>
+
+        <div id="designed-by">Designed and coded by @andersonfer</div>
+      </>
+    );
+  }
+
+  incrementBreakLength = () => {
+    this.setState((state) => ({
+      breakLength:
+        this.state.breakLength < MAX_LENGTH_IN_MINUTES
+          ? this.state.breakLength + 1
+          : this.state.breakLength
+    }));
+    //this.dispatchBreakUpdatedEvent();
+  };
+
+  decrementBreakLength = () => {
+    this.setState((state) => ({
+      breakLength:
+        this.state.breakLength > MIN_LENGTH_IN_MINUTES
+          ? this.state.breakLength - 1
+          : this.state.breakLength
+    }));
+    //this.dispatchBreakUpdatedEvent();
+  };
+
+  incrementSessionLength = () => {
+    const newSessionLength =
+      this.state.sessionLength < MAX_LENGTH_IN_MINUTES
+        ? this.state.sessionLength + 1
+        : this.state.sessionLength;
+    this.setState((state) => ({
+      sessionLength: newSessionLength
+    }));
+    this.dispatchNewSessionLength(newSessionLength);
+  };
+
+  decrementSessionLength = () => {
+    const newSessionLength =
+      this.state.sessionLength > MIN_LENGTH_IN_MINUTES
+        ? this.state.sessionLength - 1
+        : this.state.sessionLength;
+    this.setState((state) => ({
+      sessionLength: newSessionLength
+    }));
+    this.dispatchNewSessionLength(newSessionLength);
+  };
+
+  dispatchNewSessionLength = (newSessionLength) => {
+    const sessionUpdatedEvent = new CustomEvent("sessionUpdated", {
+      detail: {
+        newSessionLength: newSessionLength
+      }
+    });
+    document.dispatchEvent(sessionUpdatedEvent);
+  };
+}
+
+class BreakSetup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="break-setup" class="flex-center">
+        <div id="break-label" class="label">
+          Break Length
+        </div>
+        <div>
+          <span
+            id="break-decrement"
+            class="control"
+            onClick={this.props.decrementFnc}
+          >
+            <i class="fa-solid fa-arrow-down"></i>
+          </span>
+          <span id="break-length">{this.props.breakLength}</span>
+          <span
+            id="break-increment"
+            class="control"
+            onClick={this.props.incrementFnc}
+          >
+            <i class="fa-solid fa-arrow-up"></i>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class SessionSetup extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div id="session-setup" class="flex-center">
+        <div id="session-label" class="label">
+          Session Length
+        </div>
+        <div>
+          <span
+            id="session-decrement"
+            class="control"
+            onClick={this.props.decrementFnc}
+          >
+            <i class="fa-solid fa-arrow-down"></i>
+          </span>
+          <span id="session-length">{this.props.sessionLength}</span>
+          <span
+            id="session-increment"
+            class="control"
+            onClick={this.props.incrementFnc}
+          >
+            <i class="fa-solid fa-arrow-up"></i>
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minutes: this.props.sessionLength,
+      seconds: 0,
+      isClockPaused: true,
+      hasBreakStarted: false
+    };
+  }
+
+  componentDidMount() {
+    document.addEventListener("sessionUpdated", (e) => {
+      this.updateTimeLeft(e);
+    });
+  }
+
+  updateTimeLeft = (e) => {
+    if (this.isSessionRunning()){
+      //ToDo extract method
+      const newSessionLength = e.detail.newSessionLength;
+      this.setState({
+        minutes: newSessionLength,
+        seconds: 0
+      });
+    }//ToDo break treatment
+  };
+
+  isSessionRunning = () => {
+    return !this.state.hasBreakStarted;
+  }
+
+  componentDidUpdate() {
+    if (this.timerHasReachedZero()) {
+      this.stopClock();
+      this.playBeep();
+      //TODO this is a hack, must find another way of staying in zero
+      setTimeout(this.startSessionOrBreak,1000);
+    }
+  }
+
+  playBeep = () => {
+    document.getElementById("beep").play();
+  }
+
+  timerHasReachedZero = () => {
+    return (
+      this.state.minutes === 0 &&
+      this.state.seconds === 0 &&
+      !this.state.isClockPaused
+    );
+  };
+
+  startSessionOrBreak = () => {
+    if (this.state.hasBreakStarted) {
+        this.startSession();
+      } else {
+        this.startBreak();
+      }
+  }
+
+  startSession = () => {
+    this.setState({
+      hasBreakStarted: false,
+      minutes: this.props.sessionLength,
+      seconds: 0
+    });
+    this.resumeClock();
+  };
+
+  startBreak = () => {
+    this.setState({
+      hasBreakStarted: true,
+      minutes: this.props.breakLength,
+      seconds: 0
+    });
+    this.resumeClock();
+  };
+
+  render() {
+    return (
+      <>
+        <ClockDisplay
+          label={this.state.hasBreakStarted ? "Break" : "Session"}
+          minutes={this.state.minutes}
+          sessionLength={this.props.sessionLength}
+          seconds={this.state.seconds}
+          startStopFnc={this.startStopClock}
+          resetFnc={this.resetClock}
+        />
+      </>
+    );
+  }
+
+  startStopClock = () => {
+    if (this.state.isClockPaused) {
+      this.resumeClock();
+    } else {
+      this.stopClock();
+    }
+  };
+
+  clockID;
+  resumeClock = () => {
+    this.setState({ isClockPaused: false });
+    this.clockID = setInterval(this.updateClock, ONE_SECOND_IN_MILISECONDS);
+  };
+
+  updateClock = () => {
+    this.setState((state) => ({
+      minutes:
+        state.minutes > 0 && state.seconds === 0
+          ? state.minutes - 1
+          : state.minutes,
+      seconds:
+          state.seconds === 0
+          ? FIFTY_NINE_SECONDS
+          : state.seconds - 1
+    }));
+  };
+
+  stopClock = () => {
+    this.setState({
+      isClockPaused: true
+    });
+    clearInterval(this.clockID);
+  };
+
+  resetClock = () => {
+    this.setState({
+      hasBreakStarted: false
+    });
+    this.stopClock();
+    this.resetBeep();
+    this.dispatchResetClockEvent();
+  };
+
+  resetBeep = () => {
+    document.getElementById('beep').pause();
+    document.getElementById('beep').currentTime = 0;
+  }
+
+  dispatchResetClockEvent = () => {
+    const resetClockEvent = new CustomEvent("resetClock");
+    document.dispatchEvent(resetClockEvent);
+  };
+}
+
+class ClockDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      minutes: 0,
+      seconds: 0
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div id="clock-display" class="flex-center">
+          <div id="timer-label">{this.props.label}</div>
+          <div id="time-left">{this.getFormattedTimeLeft()}</div>
+        </div>
+        <div id="clock-controls">
+          <PlayStopButton resumeOrPauseClock={this.props.startStopFnc} />
+          <ResetButton resetClock={this.props.resetFnc} />
+          <audio id="beep" src="https://cdn.pixabay.com/download/audio/2023/01/06/audio_43c9ef7336.mp3?filename=achive-sound-132273.mp3"/>
+        </div>
+      </>
+    );
+  }
+
+  getFormattedTimeLeft = () => {
+    const formattedMinutesLeft = this.props.minutes.toLocaleString(undefined, {
+      minimumIntegerDigits: 2
+    });
+    const formattedSecondsLeft = this.props.seconds.toLocaleString(undefined, {
+      minimumIntegerDigits: 2
+    });
+    const formattedTimeLeft = formattedMinutesLeft + ":" + formattedSecondsLeft;
+    return formattedTimeLeft;
+  };
+}
+
+class PlayStopButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <span
+        id="start_stop"
+        class="control"
+        onClick={this.props.resumeOrPauseClock}
+      >
+        <i class="fa-solid fa-play"></i>
+        <i class="fa-solid fa-pause"></i>
+      </span>
+    );
+  }
+}
+
+class ResetButton extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <span id="reset" class="control" onClick={this.props.resetClock}>
+        <i class="fa-sharp fa-solid fa-repeat"></i>
+      </span>
+    );
+  }
 }
 
 export default App;
